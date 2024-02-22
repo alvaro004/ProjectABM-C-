fce8d919dff4c8b357e2e553c6c11d72cfbd4e39
alvaro004 <alvarollano04@gmail.com>
2024-02-22 16:41:38 -0300

    Starting files

---
+++ ProjectABM.DAL/ArticuloHasVenta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectABM.DAL
+{
+    public class ArticuloHasVenta
+    {
+        public int articulo_articulo_id { get; set; } // Foreign Key
+        public int venta_venta_id { get; set; } // Foreign Key
+
+        // Navigation properties (optional but recommended)
+        public Articulo Articulo { get; set; }
+        public Venta Venta { get; set; }
+    }
+}
---
+++ ProjectABM/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ProjectABM
+{
+    public partial class Form1 : Form
+    {
+        //creating the DB connection
+        private string connectionString = "Data Source=10.6.2.148:1521/dbitades;User Id=ADN;Password=1234567;";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (OracleConnection connection = new OracleConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    MessageBox.Show("Connection Successful!");
+                }
+                catch (OracleException ex)
+                {
+                    MessageBox.Show("Connection Failed: " + ex.Message);
+                }
+            }
+        }
+    }
+}
