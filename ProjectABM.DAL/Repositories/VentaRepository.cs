c28305a2231468ab02889699c04f9115c5aa4beb
alvaro004 <alvarollano04@gmail.com>
2024-02-27 14:14:42 -0300

    I created the venta repository and its interface and declare inside of them the method to fetch and retrieve all the ventas rows

--- ProjectABM.DAL/Repositories/VentaRepository.cs
+++ ProjectABM.DAL/Repositories/VentaRepository.cs
@@ -1,12 +1,45 @@
-﻿using System;
+﻿using ProjectABM.DAL.Models;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using Oracle.ManagedDataAccess.Client;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProjectABM.DAL.Repositories
 {
-    class VentaRepository
+    public class VentaRepository
     {
+
+        //FETCH AND RETRIEVE ALL THE VENTAS
+        public IEnumerable<Venta> ListVentas(OracleConnection connection)
+        {
+            var ventas = new List<Venta>();
+
+            using (var command = new OracleCommand("pkg_abm_assigment.sp_list_ventas", connection))
+            {
+                command.CommandType = CommandType.StoredProcedure;
+
+                var output = command.Parameters.Add("po_ventas", OracleDbType.RefCursor);
+                output.Direction = ParameterDirection.Output;
+
+                using (var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        var venta = new Venta
+                        {
+                            venta_id = Convert.ToInt32(reader["venta_id"]),
+                            venta_fecha = Convert.ToDateTime(reader["venta_fecha"]),
+                            cliente_cliente_id = Convert.ToInt32(reader["cliente_cliente_id"])
+                        };
+                        ventas.Add(venta);
+                    }
+                }
+            }
+
+            return ventas;
+        }
     }
 }
