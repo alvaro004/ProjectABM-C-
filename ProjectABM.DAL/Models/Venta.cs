e0be44edeb85ce52355c91d3426d9db2051736bb
alvaro004 <alvarollano04@gmail.com>
2024-02-27 13:52:52 -0300

    Venta Model added

---
+++ ProjectABM.DAL/Models/Venta.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectABM.DAL.Models
+{
+    class Venta
+    {
+    }
+}
